302ff6e144773c875ea13c12c040f9c405b6fb8c
Ivan Copeland <ivan.copeland2015@gmail.com>
2026-01-05 11:01:31 +0200

    1

--- pay/CurlClient.php
+++ pay/CurlClient.php
@@ -86,6 +86,58 @@ class CurlClient
     }
 
 
+    /**
+     * Make a POST request
+     *
+     * @param string   $url
+     * @param array    $headers
+     * @param string   $body      (already JSON-encoded)
+     * @param resource $file      (stream to write response into)
+     *
+     * @return bool|array
+     */
+    public function post($url, $headers = array(), $body = '', $file = null)
+    {
+        $this->report(sprintf('POST %s', $url));
+
+        // Always write to a real temp file
+        $tempPath = tempnam(sys_get_temp_dir(), 'curl_');
+        $tempHandle = fopen($tempPath, 'w+');
+
+        curl_setopt_array($this->curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $body,
+            CURLOPT_FILE => $tempHandle,
+        ));
+
+        $this->headers = array();
+        $result = curl_exec($this->curl);
+
+        if ($result === false) {
+            $err = curl_error($this->curl);
+            echo "<pre style='color:red'>❌ CURL POST failed: $err</pre>";
+            fclose($tempHandle);
+            unlink($tempPath);
+            return false;
+        }
+
+        $info = curl_getinfo($this->curl);
+        unset($info['local_ip'], $info['local_port']);
+        $info['headers'] = $this->headers;
+
+        fclose($tempHandle);
+
+        // Copy response into caller-provided stream
+        if ($file) {
+            $tempData = file_get_contents($tempPath);
+            fwrite($file, $tempData);
+        }
+
+        unlink($tempPath);
+        return $info;
+    }
 
 
     /**
